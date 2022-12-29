6173a6ac1b145ce77baefb20d033464a141596f9
Mariyan Momchilov <mmomchilov@asteasolutions.com>
2022-12-29 17:30:33 +0200

    add coroutine sleep and switch functionality

--- src/core/scheduler/scheduler.py
+++ src/core/scheduler/scheduler.py
@@ -1,8 +1,15 @@
 """Scheduler module."""
-
-from typing import Coroutine
+from time import time
+from typing import Coroutine, Tuple
 import heapq
 from collections import deque
+
+
+class Awaitable:
+    """Awaitable object."""
+
+    def __await__(self):
+        yield
 
 
 class Scheduler:
@@ -11,23 +18,50 @@ class Scheduler:
     def __init__(self) -> None:
         """Init."""
         self._pending: deque[Coroutine] = deque()
-        self._sleeping: list[Coroutine] = []
+        self._sleeping: list[Tuple[float, int, Coroutine]] = []
         self._current_coro: Coroutine | None = None
         self._closed = False
+        self._seq = 0
 
+    def _get_sleeping(self):
+        if self._sleeping:
+            deadline, _, coro = self._sleeping[0]
+            if time() >= deadline:
+                heapq.heappop(self._sleeping)
+                return coro
+        return None
+
+    @property
     def closed(self):
+        """Check if scheduler is closed."""
         return self._closed
 
     def close(self):
+        """Close the scheduler for further execution and scheduling."""
         self._closed = True
+        self._current_coro = None
+        for coro in self._pending:
+            coro.close()
+        for _, _, coro in self._sleeping:
+            coro.close()
 
     def call_soon(self, coro: Coroutine):
         """Schedules coroutine for execution."""
         self._pending.append(coro)
 
+    def call_later(self, coro: Coroutine, seconds: int):
+        """Schedules coroutine for execution after :seconds."""
+        if seconds < 0:
+            raise ValueError('seconds must be non negative')
+        heapq.heappush(self._sleeping, (time() + seconds, self._seq, coro))
+        self._seq += 1
+
     def run(self) -> None:
         """Run the scheduler."""
         while not self._closed:
+            coro = self._get_sleeping()
+            if coro is not None:
+                self._pending.append(coro)
             if not self._pending:
                 continue
             try:
@@ -39,3 +73,18 @@ class Scheduler:
                 pass
             except Exception:
                 raise
+
+    async def switch(self):
+        """Suspend current execution of coroutine."""
+        await Awaitable()
+
+    async def sleep(self, seconds: int = 0):
+        """Suspend current execution of coroutine for :seconds."""
+        if seconds < 0:
+            raise ValueError('seconds must be non negative')
+
+        # condition is always true (mypy)
+        if self._current_coro is not None:
+            self.call_later(self._current_coro, seconds)
+        self._current_coro = None
+        await self.switch()
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,4 +1,5 @@
 from types import coroutine
+from collections import deque
 from src.core.scheduler import Scheduler
 
 
@@ -14,32 +15,107 @@ def close_counter_handler(counter: int):
     return close_sched
 
 
-signal_close = close_counter_handler(2)
-
-
 @coroutine
-def _countup(to: int, sched: Scheduler):
+def _countup(to: int, sched: Scheduler, handler):
     i = 0
     while i < to:
         count_container.append(i)
         yield i
         i += 1
-    signal_close(sched)
+    handler(sched)
 
 
 @coroutine
-def _countdown(to: int, sched: Scheduler):
+def _countdown(to: int, sched: Scheduler, handler):
     i = to - 1
     while i >= 0:
         count_container.append(i)
         yield i
         i -= 1
-    signal_close(sched)
+    handler(sched)
 
 
 def test_scheduler_only_call_soon():
+    global count_container
+    count_container = []
+    signal_close = close_counter_handler(2)
     sched = Scheduler()
-    sched.call_soon(_countup(5, sched))
-    sched.call_soon(_countdown(5, sched))
+    sched.call_soon(_countup(5, sched, signal_close))
+    sched.call_soon(_countdown(5, sched, signal_close))
     sched.run()
     assert count_container == [0, 4, 1, 3, 2, 2, 3, 1, 4, 0]
+
+
+def test_scheduler_call_later():
+    global count_container
+    count_container = []
+    sched = Scheduler()
+    signal_close = close_counter_handler(2)
+    sched.call_later(_countup(5, sched, signal_close), 1)
+    sched.call_soon(_countdown(5, sched, signal_close))
+    sched.run()
+    assert count_container == [4, 3, 2, 1, 0, 0, 1, 2, 3, 4]
+
+
+class TestException(Exception):
+    pass
+
+
+async def throw_exception():
+    raise TestException()
+
+
+def test_sched_exception_handling():
+    sched = Scheduler()
+    sched.call_soon(throw_exception())
+    try:
+        sched.run()
+    except TestException:
+        assert True
+        return
+    assert False
+
+
+logs = []
+sched = Scheduler()
+
+
+async def stop_scheduler():
+    while sched._pending or sched._sleeping:
+        await sched.switch()
+    sched.close()
+
+
+async def log_twice(string: str):
+    logs.append(string + '(1)')
+    await sched.switch()
+    logs.append(string + '(2)')
+
+
+def test_sched_switch():
+    sched.call_soon(log_twice('First'))
+    sched.call_soon(log_twice('Second'))
+    sched.call_soon(stop_scheduler())
+    sched.run()
+    assert logs == ['First(1)', 'Second(1)', 'First(2)', 'Second(2)']
+
+
+async def log_sleep_twice(string: str, sleep_time=1):
+    logs.append(string + '(1)')
+    await sched.sleep(sleep_time)
+    logs.append(string + '(2)')
+
+
+def test_sched_sleep():
+    global logs
+    logs = []
+    global sched
+    sched = Scheduler()
+    sched._closed = False
+    sched._pending = deque()
+    sched._sleeping = []
+    sched.call_soon(log_sleep_twice('First', 2))
+    sched.call_soon(log_sleep_twice('Second', 1))
+    sched.call_soon(stop_scheduler())
+    sched.run()
+    assert logs == ['First(1)', 'Second(1)', 'Second(2)', 'First(2)']
